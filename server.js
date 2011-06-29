02e0acc9f5f7c5f353bdd6ff7cea8d204dc24d45
Daniel <daniel@cloudspace.com>
2011-06-29 04:02:13 -0400

    asdf

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ socket.on('connection', function(client) {
 
   client.on('message', function(message){
     console.log(message);
-    var value_in_hex = parseInt(message.slider_value).toString(16);
+    var value_in_hex = (parseInt(message.slider_value)*2.55).toString(16);
     var buffer = new Buffer(value_in_hex + "00000000000000\x0d\x0a");
     lightswitch_socket.send(buffer, 0, 18, 9802, '97.102.15.225', function(a,b,c) { console.log(a,b,c);});
   });
