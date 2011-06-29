1531b41d25029e9f19f23578fd53adc6e405033f
Daniel <daniel@cloudspace.com>
2011-06-29 03:42:31 -0400

    adding udp supppoorrrttt

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var sys = require("sys");
 var util = require('util');
 var io = require("socket.io");
+var dgram = require('dgram');
+
+var lightswitch_socket = dgram.createSocket("udp4");
 
 app = express.createServer();
 
@@ -23,6 +26,7 @@ socket.on('connection', function(client) {
 
   client.on('message', function(message){
     console.log(message);
+    lightswitch_socket.send('FF', 0, 2, 9802, '97.102.15.225', function(a,b,c) { console.log(a,b,c);});
   });
 });
 
