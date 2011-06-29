16d52f815410228ab88eb762adfacd3c14316b90
Ubuntu <ubuntu@ip-10-99-87-0.ec2.internal>
2011-06-29 07:13:31 +0000

    adding nodejs stuff from super multipler pong

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var sys = require("sys");
+var util = require('util');
+var io = require("socket.io");
+
+app = express.createServer();
+
+app.listen(80);
+
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+
+var socket = io.listen(app);
+
+socket.on('connection', function(client) {
+
+  client.on('message', function(message){
+  });
+});
+
+
