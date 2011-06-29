48573f00ee77303b0dc14a91fbf13fa66ba634de
Daniel <daniel@cloudspace.com>
2011-06-29 02:56:22 -0400

    removed more unnecessary things, added dragend event that doesnt actually work. how does senchatouchs slider object actually take event bindings?

--- src/index.js
+++ src/index.js
@@ -4,123 +4,18 @@ Ext.setup({
     phoneStartupScreen: 'phone_startup.png',
     glossOnIcon: false,
     onReady: function() {
+       var form;
 
-        var form;
-        
-        Ext.regModel('User', {
-            fields: [
-                {name: 'name',     type: 'string'},
-                {name: 'password', type: 'password'},
-                {name: 'email',    type: 'string'},
-                {name: 'url',      type: 'string'},
-                {name: 'rank',     type: 'string'},
-                {name: 'enable',   type: 'boolean'},
-                {name: 'cool',     type: 'boolean'},
-                {name: 'color',    type: 'string'},
-                {name: 'team',     type: 'string'},
-                {name: 'secret',   type: 'boolean'}
-            ]
-        });
-        
-         Ext.regModel('Ranks', {
-            fields: [
-                {name: 'rank',     type: 'string'},
-                {name: 'title',    type: 'string'}
-            ]
-         });
-        
-        var ranksStore = new Ext.data.JsonStore({
-           data : [
-                { rank : 'master',  title : 'Master'},
-                { rank : 'padawan', title : 'Student'},
-                { rank : 'teacher', title : 'Instructor'},
-                { rank : 'aid',     title : 'Assistant'}
-           ],
-           model : 'Ranks',
-           autoLoad : true,
-           autoDestroy : true
-        });
-        
-        var formBase = {
-            scroll: 'vertical',
-            url   : 'postUser.php',
-            standardSubmit : false,
-            items: [{
-                    xtype: 'fieldset',
-                    title: 'Personal Info',
-                    instructions: 'Please enter the information above.',
-                    defaults: {
-                        required: true,
-                        labelAlign: 'left',
-                        labelWidth: '40%'
-                    },
-                    items: [
-                    {
-                        xtype: 'sliderfield',
-                        name : 'height',
-                        label: 'Height'
-                    }]
-                }
-            ],
-            listeners : {
-                submit : function(form, result){
-                    console.log('success', Ext.toArray(arguments));
-                },
-                exception : function(form, result){
-                    console.log('failure', Ext.toArray(arguments));
-                }
-            },
-        
-            dockedItems: [
-                {
-                    xtype: 'toolbar',
-                    dock: 'bottom',
-                    items: [
-                        {
-                            text: 'Load Model',
-                            ui: 'round',
-                            handler: function() {
-                                formBase.user = Ext.ModelMgr.create({
-                                    'name'    : 'Akura',
-                                    'password': 'secret',
-                                    'email'   : 'saru@sencha.com',
-                                    'url'     : 'http://sencha.com',
-                                    'single_slider': 20,
-                                    'enable'  : 1,
-                                    'cool'    : true,
-                                    'team'    : 'redteam',
-                                    'color'   : 'blue',
-                                    'rank'    : 'padawan',
-                                    'secret'  : true,
-                                    'bio'     : 'Learned the hard way !'
-                                }, 'User');
-        
-                                form.loadModel(formBase.user);
-                            }
-                        },
-                        {xtype: 'spacer'},
-                        {
-                            text: 'Reset',
-                            handler: function() {
-                                form.reset();
-                            }
-                        },
-                        {
-                            text: 'Save',
-                            ui: 'confirm',
-                            handler: function() {
-                                if(formBase.user){
-                                    form.updateRecord(formBase.user, true);
-                                }
-                                form.submit({
-                                    waitMsg : {message:'Submitting', cls : 'demos-loading'}
-                                });
-                            }
-                        }
-                    ]
-                }
-            ]
-        };
+       var formBase = {
+           items: [
+           {
+               xtype: 'sliderfield',
+               name : 'height',
+               label: 'Height',
+               dragend: function(s,t,v) { console.log(s,t,v); }
+           }]
+  
+       };
         
         if (Ext.is.Phone) {
             formBase.fullscreen = true;
